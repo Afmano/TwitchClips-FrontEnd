b9d84cbc3f7f262b0586271eb42817f257048cdf
Alpritor <alpritor@outlook.com>
2025-01-31 22:39:10 +0200

    Project setup and biome addition.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import type { Routes } from "@angular/router";
+import { IndexComponent } from "./index/index.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "/index", component: IndexComponent },
+	{ path: "**", component: NotFoundComponent },
+];
